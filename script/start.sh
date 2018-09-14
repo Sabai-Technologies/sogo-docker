c5b642ab4f6ff8786043818188a188fdb43b86b6
Tophe <christophe@amory.fr>
2018-09-14 20:26:22 +0200

    init MySQL database only if MYSQL_SERVER is filled

--- script/start.sh
+++ script/start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 source /usr/local/bin/common.sh
-source /usr/local/bin/mysql.sh
 
 for conf in $(printenv| grep -i SOGO_ | cut -d= -f1);do
     update_conf "${conf:5}" "${!conf}"
 done
 
-wait_for_db
-init_db
+if [[ -n ${MYSQL_SERVER//[[:blank:]]/} ]]; then
+    source /usr/local/bin/mysql.sh
+    wait_for_db
+    init_db
+fi
 
 log_info "Launching SOGo"
 /usr/sbin/sogod -WONoDetach YES -WOPort 20000 -WOLogFile - -WOPidFile /tmp/sogo.pid
